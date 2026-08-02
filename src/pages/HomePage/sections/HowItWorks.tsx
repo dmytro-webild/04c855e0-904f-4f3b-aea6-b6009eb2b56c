8552e85d6849ec4ff810af26b65aaa0f5cd79f7f
bender <kudin.dima@gmail.com>
2026-08-02 01:39:17 +0000

    Update src/pages/HomePage/sections/HowItWorks.tsx

--- src/pages/HomePage/sections/HowItWorks.tsx
+++ src/pages/HomePage/sections/HowItWorks.tsx
@@ -31,7 +31,7 @@ export default function HowItWorksSection(): React.JSX.Element {
               description: "Pick your date and time, and get ready to turn heads with your new look.",
             },
           ]}
-          imageSrc="https://storage.googleapis.com/webild/users/user_3H6bWYFe0S1ZtUhWnfj8Arfxk1E/uploaded-1785631814586-vy36swkf.jpg"
+          imageSrc="https://storage.googleapis.com/webild/users/user_3H6bWYFe0S1ZtUhWnfj8Arfxk1E/uploaded-1785633413914-xu7co0ax.jpg"
           textAnimation="slide-up"
         />
         </SectionErrorBoundary>
